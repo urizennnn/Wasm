94d803121651e53282f33c4076be56a576e80644
urizennnn <igamerryt@gmail.com>
2024-03-20 17:05:57 +0100

    added insertion sort

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	// fmt.Println("Hello World")
 	arr:=[]int{10,6,5,7,8,0}
 	// fmt.Print(sortingAlgorithms.Quicksort(arr))
-	fmt.Print(sortingAlgorithms.Bubblesort(arr))
+	//fmt.Print(sortingAlgorithms.Bubblesort(arr))
+	fmt.Print(sortingAlgorithms.Insertionsort(arr))
 }
