53fe4ffad5c65aa784a5215270278b603671ce31
urizennnn <igamerryt@gmail.com>
2024-03-20 08:51:47 +0100

    optimised bubble-sort code

--- sortingAlgorithms/bubblesort.go
+++ sortingAlgorithms/bubblesort.go
@@ -10,14 +10,16 @@ func Bubblesort (arr[]int)[]int{
 	}
 
 	for i:=0; i<length - 1; i++ {
+		flag := 0
 		for j:=0; j<length-1-i;j++{
+			flag = 1
 			if arr[j] > arr[j+1]{
 				temp:=arr[j]
 				arr[j] = arr[j+1]
 				arr[j+1] = temp
 			}
 		}
-
+		if flag == 0{ break}
 	}
 	return arr
 }
